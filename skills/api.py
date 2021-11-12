dd110927df1f44260f2f179e407e09709572957c
Quentin Santos <qsantos@qsantos.fr>
2021-11-12 20:11:24 +0100

    Basic API

---
+++ skills/api.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+from flask import Blueprint, Flask
+from flask_restplus import Api, Resource
+from jsonschema import FormatChecker
+
+from skills.schema import Belt, session_context
+
+api_blueprint = Blueprint('api', __name__)
+api = Api(
+    api_blueprint,
+    title='Skills API',
+    format_checker=FormatChecker(),
+    base_url='/api',
+)
+
+belts_ns = api.namespace('belts', path='/')
+
+@belts_ns.route('/belts')
+class BeltsResource(Resource):
+    def get(self) -> Any:
+        with session_context() as session:
+            return [
+                belt.name
+                for belt in session.query(Belt)
+            ]
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.register_blueprint(api_blueprint, url_prefix='/api')
+    return app
