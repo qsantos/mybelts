2d165540c34b7d394df86eae6070ddca89cc8e2e
Quentin Santos <qsantos@qsantos.fr>
2021-11-16 18:46:00 +0100

    Fix missing ts files for new API endpoints

---
+++ front/src/api/models/BeltAttemptPut.ts
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type BeltAttemptPut = {
+    belt_id?: number;
+    skill_domain_id?: number;
+    student_id?: number;
+    success?: boolean;
+}
---
+++ front/src/api/services/BeltAttemptsService.ts
@@ -0,0 +1,47 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { BeltAttemptOne } from '../models/BeltAttemptOne';
+import type { BeltAttemptPut } from '../models/BeltAttemptPut';
+import type { CancelablePromise } from '../core/CancelablePromise';
+import { request as __request } from '../core/request';
+
+export class BeltAttemptsService {
+
+    /**
+     * @param beltAttemptId
+     * @returns any Success
+     * @throws ApiError
+     */
+    public static deleteBeltAttemptsResource(
+        beltAttemptId: number,
+    ): CancelablePromise<any> {
+        return __request({
+            method: 'DELETE',
+            path: `/belt-attempts/${beltAttemptId}`,
+        });
+    }
+
+    /**
+     * @param beltAttemptId
+     * @param payload
+     * @param xFields An optional fields mask
+     * @returns BeltAttemptOne Success
+     * @throws ApiError
+     */
+    public static putBeltAttemptsResource(
+        beltAttemptId: number,
+        payload: BeltAttemptPut,
+        xFields?: string,
+    ): CancelablePromise<BeltAttemptOne> {
+        return __request({
+            method: 'PUT',
+            path: `/belt-attempts/${beltAttemptId}`,
+            headers: {
+                'X-Fields': xFields,
+            },
+            body: payload,
+        });
+    }
+
+}
