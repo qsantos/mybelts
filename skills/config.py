912b367e241da28cdda894f34119c8739e5adb07
Quentin Santos <qsantos@qsantos.fr>
2021-11-19 20:45:01 +0100

    Add CI

--- skills/config.py
+++ skills/config.py
@@ -1,3 +1,11 @@
-POSTGRES_URI = 'postgresql+psycopg2://skills:skills@localhost/skills'
+from os import environ
+
+PGUSER = environ.get('PGUSER', 'skills')
+PGPASSWORD = environ.get('PGPASSWORD', 'skills')
+PGHOST = environ.get('PGHOST', 'localhost')
+PGPORT = environ.get('PGPORT', '5432')
+PGSCHEMA = environ.get('PGSCHEMA', 'skills')
+
+POSTGRES_URI = f'postgresql+psycopg2://{PGUSER}:{PGPASSWORD}@{PGHOST}:{PGPORT}/{PGSCHEMA}'
 
 SECRET = 'some_secret'
