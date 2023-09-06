0111f6ce87b702be2a43b10df90ebed3ff32f7c8
Quentin Santos <qsantos@qsantos.fr>
2023-09-06 18:45:09 +0200

    front: fix unused import

--- front/src/SchoolClassWaitlist.tsx
+++ front/src/SchoolClassWaitlist.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ReactElement, useState } from 'react';
+import { ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
 import Alert from 'react-bootstrap/Alert';
 
